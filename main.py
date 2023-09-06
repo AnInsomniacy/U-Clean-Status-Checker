c3034666d6274a13ad5b9434a0d0e35817c2dcaa
sekiro <qq1326555262@gmail.com>
2023-09-07 00:53:44 +0800

    更新架构 从PyQt5更新到PyQt6

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication
-
 from GUI import *
 import sys
 
 if __name__ == "__main__":
-    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    # 启动GUI
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
